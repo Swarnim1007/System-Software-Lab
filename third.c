ea3643c4f5feaef32a66a744a12d64a3ba3861ae
swarnim <swarnimkukreti@gmail.com>
2023-08-12 15:02:58 +0530

    second and third of hands on list 1

---
+++ third.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<fcntl.h>
+#include<unistd.h>
+int main()
+{
+	int fd=creat("example.txt",S_IRUSR | S_IWUSR);
+
+	if(fd==-1)
+	{perror("Error on creating file");
+		return 1;
+	}
+	printf("file created successfully");
+
+	if(close(fd)==-1)
+	{
+		perror("Errorr on creating file");
+		return 1;
+	}
+	return 0;
+}
