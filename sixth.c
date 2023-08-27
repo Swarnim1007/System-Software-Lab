2f247f18565ac2cb53d74a499189fbdeef4119c0
swarnim <swarnimkukreti@gmail.com>
2023-08-27 21:53:31 +0530

    updated till 18

--- sixth.c
+++ sixth.c
@@ -1,6 +1,21 @@
+#include<stdio.h>
 #include<unistd.h>
 
-int main()
-{
-	char buffer[1024];
+int main() {
+    char buffer[1024];
+
+    size_t bytesRead;
+    while ((bytesRead = read(STDIN_FILENO, buffer, sizeof(buffer))) > 1) {
+        
+        write(STDOUT_FILENO, buffer, bytesRead);
+    }
+
+    if (bytesRead == -1) {
+        perror("Error reading from STDIN");
+        return 1;
+    }
+
+    return 0;
+
+}
 
