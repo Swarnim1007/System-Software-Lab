aa8158013906366edb0b756362fd6e255efb0468
swarnim <swarnimkukreti@gmail.com>
2023-10-11 17:02:04 +0530

    updated

--- hands_on_list2/30/30.c
+++ hands_on_list2/30/30.c
@@ -10,62 +10,58 @@ d. remove the shared memory
  Date: 10th OCT, 2023.
 ============================================================================
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
 #include <sys/shm.h>
 
-#define SHM_KEY 12345
-#define SHM_SIZE 1024
-
 int main() {
-    int shmid;
-    char *shmaddr;
-    char data[] = "Hello, shared memory!";
+    int shm_id;
+    key_t key = ftok("shared_memory_example", 65); // Generate a key for the shared memory
 
     // Create shared memory segment
-    shmid = shmget(SHM_KEY, SHM_SIZE, 0666 | IPC_CREAT);
-    if (shmid == -1) {
-        perror("shmget");
+    shm_id = shmget(key, 1024, IPC_CREAT | 0666);
+    if (shm_id == -1) {
+        perror("shmget failed");
         exit(EXIT_FAILURE);
     }
 
-    // Attach shared memory segment with read-write permission
-    shmaddr = shmat(shmid, NULL, 0);
-    if (shmaddr == (char *)-1) {
-        perror("shmat");
+    // Attach shared memory segment
+    char *data = (char *)shmat(shm_id, NULL, 0);
+    if (data == (char *)(-1)) {
+        perror("shmat failed");
         exit(EXIT_FAILURE);
     }
 
     // Write data to shared memory
-    strcpy(shmaddr, data);
-    printf("Data written to shared memory: %s\n", shmaddr);
+    strcpy(data, "Hello, shared memory!");
 
-    // Attach shared memory segment with read-only permission
-    char *readonly_shmaddr = shmat(shmid, NULL, SHM_RDONLY);
-    if (readonly_shmaddr == (char *)-1) {
-        perror("shmat (read-only)");
+    printf("Data written to shared memory: %s\n", data);
+
+    // Detach shared memory segment with read-only permissions
+    shmdt(data);
+
+    // Try to overwrite the data (this will fail because it's read-only)
+    data = (char *)shmat(shm_id, NULL, SHM_RDONLY);
+    if (data == (char *)(-1)) {
+        perror("shmat failed (read-only)");
     } else {
-        // Attempt to overwrite data in read-only attached memory (this should fail)
-        strcpy(readonly_shmaddr, "This should fail!");
+        strcpy(data, "This will not work!");
+        printf("Attempted to overwrite: %s\n", data);
+        shmdt(data);
     }
 
-    // Detach shared memory
-    if (shmdt(shmaddr) == -1) {
-        perror("shmdt");
-        exit(EXIT_FAILURE);
-    }
+    // Detach shared memory segment
+    shmdt(data);
 
     // Remove shared memory segment
-    if (shmctl(shmid, IPC_RMID, NULL) == -1) {
-        perror("shmctl (IPC_RMID)");
-        exit(EXIT_FAILURE);
-    }
+    shmctl(shm_id, IPC_RMID, NULL);
 
-    printf("Shared memory detached and removed successfully.\n");
+    printf("Shared memory removed\n");
 
     return 0;
 }
-
 
